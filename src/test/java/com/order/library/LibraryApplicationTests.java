3ef102db414c56f91adb48a6a3c1fe4636b2160f
ArsenYeritsyan <yeritsyan.arsen@gmail.com>
2023-08-04 16:39:36 +0400

    first

---
+++ src/test/java/com/order/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
